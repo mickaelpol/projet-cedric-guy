7b6c661ba4160d45a21cf115656d2c5c80831fd9
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-08-20 17:46:39 +0200

    amelioration affichage des paragraphes et image sur le clic fleche

--- app.js
+++ app.js
@@ -1,83 +1,91 @@
-$('document').ready(function(){
+$('#caché').hide();
 
-	// console.log("salut salut");
+$('#photo1').hide();
+$('#caché2').hide();
+
+$('#photo2').hide();
+$('#caché3').hide();
+
+$(function(){
+
+
 	var count = 0;
 	var idTitre = $('#clicker');
 	var textCaché2 = "Votre projet en étapes: <br>\
-              <br>\
-              -Présentation de votre projet <br>\
-              -Rendez vous technique de votre projet <br>\
-              -Présentation des devis <br>\
-              -Démarrage des travaux <br>\
-              -Appréciation de la prestation <br>\
-              Les services de votre courtier en travaux sont :\
-              GRATUITS";
-var photoCaché = '<img  src="image/icone/votre-projet.png" width="250" height="200" class="center-block img-responsive img-thumbnail" alt="logo projet">';
-var photoCaché2 = '<img src="image/icone/gestion-administrative.jpg" width="250" height="200" class="center-block img-responsive img-thumbnail" alt="logo projet">';
+  <br>\
+  -Présentation de votre projet <br>\
+  -Rendez vous technique de votre projet <br>\
+  -Présentation des devis <br>\
+  -Démarrage des travaux <br>\
+  -Appréciation de la prestation <br>\
+  Les services de votre courtier en travaux sont :\
+  GRATUITS";
+  var photoCaché = '<img  src="image/icone/votre-projet.png" width="250" height="200" class="center-block img-responsive img-thumbnail" alt="logo projet">';
+  var photoCaché2 = '<img src="image/icone/gestion-administrative.jpg" width="250" height="200" class="center-block img-responsive img-thumbnail" alt="logo projet">';
 
   var textCaché = "\
   Bienvenue chez : Action Travaux Occitanie\
-\
-              Nous sommes ravis de vous accueillir sur notre site. Notre société vous offre un grand choix de services parfaitement adaptés à vos exigences.\
-\
-\
-\
-              Vous souhaitez en savoir plus sur notre gamme de services ? Consultez notre offre.\
-\
-              Faites connaissance avec notre entreprise et notre équipe. Nous sommes à votre disposition pour toute demande ou question. N'hésitez pas à nous contacter !<br> <br>\
-              ACTION TRAVAUX OCCITANIE <br>\
-              est une société de courtage en travaux qui est spécialisé dans les travaux de rénovation intérieure ou extérieure, que ce soit pour carreler une cuisine, rénover une salle de bain ou une réfection de la maison de A à Z. <br>\
-              action travaux occitanie\
-              s'occupe également des travaux de construction ou encore de l'aménagement extérieur (jardin, terrasse)."
+  \
+  Nous sommes ravis de vous accueillir sur notre site. Notre société vous offre un grand choix de services parfaitement adaptés à vos exigences.\
+  \
+  \
+  \
+  Vous souhaitez en savoir plus sur notre gamme de services ? Consultez notre offre.\
+  \
+  Faites connaissance avec notre entreprise et notre équipe. Nous sommes à votre disposition pour toute demande ou question. N'hésitez pas à nous contacter !<br> <br>\
+  ACTION TRAVAUX OCCITANIE <br>\
+  est une société de courtage en travaux qui est spécialisé dans les travaux de rénovation intérieure ou extérieure, que ce soit pour carreler une cuisine, rénover une salle de bain ou une réfection de la maison de A à Z. <br>\
+  action travaux occitanie\
+  s'occupe également des travaux de construction ou encore de l'aménagement extérieur (jardin, terrasse)."
 
-var textCaché3 = "ACTION TRAVAUX OCCITANIE\
-                peut vous informer ou vous orienter vis à vis des aides, crédits d'impôts, normes et lois en vigueur afférentes à vos projets de travaux.\
-                Un accompagnement pour monter vos dossiers de financement.\
-                Transmettre les informations sur les assurances obligatoires et conseillées.";
-
-
+  var textCaché3 = "ACTION TRAVAUX OCCITANIE\
+  peut vous informer ou vous orienter vis à vis des aides, crédits d'impôts, normes et lois en vigueur afférentes à vos projets de travaux.\
+  Un accompagnement pour monter vos dossiers de financement.\
+  Transmettre les informations sur les assurances obligatoires et conseillées.";
 
 
 
 
-              $('#clicker').click(function(){
-              	count++;
-       			// console.log(count);
-
-              	if (count%1 == 0){
-              		$('#caché').html(textCaché).show('slow');
-              	} if (count%2 == 0) {
-              		$('#caché').html(textCaché).hide('slow');
-              	}
-              });
 
 
-           	 $('#click2').click(function(){
-             	count++;
+  $('#clicker').click(function(){
+   count++;
+       			//console.log(count);
+
+             if (count%1 == 0){
+              $('#caché').html(textCaché).show('slow');
+            } if (count%2 == 0) {
+              $('#caché').html(textCaché).hide('slow');
+            }
+          });
+
+
+  $('#click2').click(function(){
+    count++;
       			// console.log(count);
 
-             	if (count%1 == 0){
-                $('#photo1').html(photoCaché).show('slow');
-             		$('#caché2').html(textCaché2).show('slow');
-             	} if (count%2 == 0) {
-                $('#photo1').html(photoCaché).hide('slow');
-             		$('#caché2').html(textCaché2).hide('slow');
-             	}
-             });
+            if (count%1 == 0){
+              $('#photo1').html(photoCaché).show('slow');
+              $('#caché2').html(textCaché2).show('slow');
+            } if (count%2 == 0) {
+              $('#photo1').html(photoCaché).hide('slow');
+              $('#caché2').html(textCaché2).hide('slow');
+            }
+          });
 
-             $('#click3').click(function(){
-             	count++;
+  $('#click3').click(function(){
+    count++;
       			// console.log(count);
 
-             	if (count%1 == 0){
-                $('#photo2').html(photoCaché2).show('slow');
-             		$('#caché3').html(textCaché3).show('slow');
-             	} if (count%2 == 0) {
-                $('#photo2').html(photoCaché2).hide('slow');
-             		$('#caché3').html(textCaché3).hide('slow');
-             	}
-             });
- });
+            if (count%1 == 0){
+              $('#photo2').html(photoCaché2).show('slow');
+              $('#caché3').html(textCaché3).show('slow');
+            } if (count%2 == 0) {
+              $('#photo2').html(photoCaché2).hide('slow');
+              $('#caché3').html(textCaché3).hide('slow');
+            }
+          });
+});
 
 
 /* quand je clique sur le bouton .clic qui correspond a une section 
@@ -89,7 +97,7 @@ var textCaché3 = "ACTION TRAVAUX OCCITANIE\
 
 
 
-/* ----------------------------------------------------------------------------------------------------*/
+      /* ----------------------------------------------------------------------------------------------------*/
 
 //test d'un refacoring 
 
